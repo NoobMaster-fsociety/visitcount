32a86c4998c610aaa1e3eb2e2c14fe6fcfc94aa0
Franz Maneclang <104089786+NoobMaster-fsociety@users.noreply.github.com>
2023-09-21 15:12:46 +0800

    Update server.js

--- server.js
+++ server.js
@@ -7,8 +7,10 @@ let visitorCount = 0;
 app.get("/api/visitor-count", (req, res) => {
   // Simulate a visitor count increase for demonstration purposes
   visitorCount += 1;
+  console.log(`Visitor count increased to ${visitorCount}`);
   res.json({ count: visitorCount });
 });
+
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
