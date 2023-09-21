4a5f1637142461bd11aa17c58c56a8614fa526ec
Franz Maneclang <104089786+NoobMaster-fsociety@users.noreply.github.com>
2023-09-21 15:06:35 +0800

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const port = 5000;
+const port = process.env.PORT || 5000; // Use the provided port or default to 5000
 
 let visitorCount = 0;
 
