f0c5300eb0249b8716be50646f0869512fe8ef03
Franz Maneclang <104089786+NoobMaster-fsociety@users.noreply.github.com>
2023-09-22 10:14:12 +0800

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import VisitorCounter from "./App";
+
+ReactDOM.render(
+  <React.StrictMode>
+    <VisitorCounter />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
