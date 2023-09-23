ec6e4985f7263a13d46d7073a4392451cee21784
Franz Maneclang <104089786+NoobMaster-fsociety@users.noreply.github.com>
2023-09-23 22:47:33 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,36 +1,47 @@
 import React, { useState, useEffect } from 'react';
 
 function App() {
-  const [counterData, setCounterData] = useState(null);
+  const [counterValue, setCounterValue] = useState(null);
 
   useEffect(() => {
-    // Fetch data from the first source
-    fetch('https://www.free-counters.org/count/d1v9')
-      .then((response) => response.json())
-      .then((data) => {
-        // Handle data from the first source
-        setCounterData(data);
+    // Fetch data from the second source
+    fetch('https://www.freevisitorcounters.com/en/home/counter/1078761/t/2')
+      .then((response) => response.text())
+      .then((html) => {
+        // Extract the counter value from the HTML content
+        const parser = new DOMParser();
+        const doc = parser.parseFromString(html, 'text/html');
+        const counterElement = doc.querySelector('.countElement');
+
+        if (counterElement) {
+          const counterText = counterElement.textContent;
+          // Assuming the counter text is a number, you can parse it as needed
+          const counter = parseInt(counterText, 10);
+          setCounterValue(counter);
+        }
       })
       .catch((error) => {
         console.error('Error fetching data:', error);
       });
-
-    // Fetch data from the second source
-    // You can use a similar approach for the second source
   }, []);
 
   return (
     <div>
-      {counterData ? (
-        <div>
-          <p>Counter Value: {counterData.count}</p>
-          {/* Render other data as needed */}
-        </div>
-      ) : (
-        <p>Loading data...</p>
-      )}
+      <p>
+        <a href='https://www.free-counters.org/'>Counter</a>
+      </p>
+      <p>Counter Value: {counterValue !== null ? counterValue : 'Loading data...'}</p>
+      <script
+        type='text/javascript'
+        src='https://www.freevisitorcounters.com/auth.php?id=2026f6bb4c32910cb8b14fac0aabcbb9b26c2cbe'
+      ></script>
+      <script
+        type='text/javascript'
+        src='https://www.freevisitorcounters.com/en/home/counter/1078761/t/2'
+      ></script>
     </div>
   );
 }
+
 
 export default App;
