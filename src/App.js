ea771d423e0bb43fe6f5e94db4e226eb5c43eaca
Franz Maneclang <104089786+NoobMaster-fsociety@users.noreply.github.com>
2023-09-23 22:57:38 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,45 +1,26 @@
-import React, { useState, useEffect } from 'react';
+import { useEffect, useState } from "react";
+import "./styles.css";
 
-function App() {
-  const [counterValue, setCounterValue] = useState(null);
-
-  useEffect(() => {
-    // Fetch data from the second source
-    fetch('https://www.freevisitorcounters.com/en/home/counter/1078761/t/2')
-      .then((response) => response.text())
-      .then((html) => {
-        // Extract the counter value from the HTML content
-        const parser = new DOMParser();
-        const doc = parser.parseFromString(html, 'text/html');
-        const counterElement = doc.querySelector('.countElement');
-
-        if (counterElement) {
-          const counterText = counterElement.textContent;
-          // Assuming the counter text is a number, you can parse it as needed
-          const counter = parseInt(counterText, 10);
-          setCounterValue(counter);
-        }
-      })
-      .catch((error) => {
-        console.error('Error fetching data:', error);
-      });
-  }, []);
-
+export default function App() {
   return (
-    <div>
-  
-      <p>Counter Value: {counterValue !== null ? counterValue : 'Loading data...'}</p>
-      <script
-        type='text/javascript'
-        src='https://www.freevisitorcounters.com/auth.php?id=2026f6bb4c32910cb8b14fac0aabcbb9b26c2cbe'
-      ></script>
-      <script
-        type='text/javascript'
-        src='https://www.freevisitorcounters.com/en/home/counter/1078761/t/2'
-      ></script>
+    <div className="App">
+      <VisitCounter />
     </div>
   );
 }
 
+function VisitCounter() {
+  const [visits, setVisits] = useState(0);
 
-export default App;
+  // Loading from localStorage
+  useEffect(() => {
+    const storedVisits = Number(localStorage.getItem("visitCounter")) || 0;
+    setVisits(storedVisits + 1);
+  }, []);
+
+  // Saving in localStorage
+  useEffect(() => {
+    localStorage.setItem("visitCounter", visits);
+  }, [visits]);
+
+  return <p>{visits}</p>;
