69c59b1585b09057898fdb0a28698c8d302ace12
Franz Maneclang <104089786+NoobMaster-fsociety@users.noreply.github.com>
2023-09-23 21:41:40 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,26 +2,6 @@ import React, { useEffect, useState } from "react";
 import "./styles.css";
 
 export default function App() {
-  return (
-    <div className="App">
-      <VisitCounter />
-    </div>
-  );
-}
-
-function VisitCounter() {
-  const [visits, setVisits] = useState(0);
-
-  // Loading from localStorage
-  useEffect(() => {
-    const storedVisits = Number(localStorage.getItem("visitCounter")) || 0;
-    setVisits(storedVisits + 1);
-  }, []);
-
-  // Saving in localStorage
-  useEffect(() => {
-    localStorage.setItem("visitCounter", visits);
-  }, [visits]);
 
   return (
     <div>
