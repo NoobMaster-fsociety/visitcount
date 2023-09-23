e58e782c7596d482948507f30cf3bddc01efb139
Franz Maneclang <104089786+NoobMaster-fsociety@users.noreply.github.com>
2023-09-23 22:37:45 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,27 +1,34 @@
 import React, { useState, useEffect } from 'react';
 
 function App() {
-  const [counterValue, setCounterValue] = useState(null);
+  const [counterData, setCounterData] = useState(null);
 
   useEffect(() => {
-    // Replace this URL with the actual URL of your counter data
-    const counterDataURL = "https://www.freevisitorcounters.com/api/counter/1078757";
-
-    fetch(counterDataURL)
-      .then(response => response.json())
-      .then(data => {
-        const value = data.value; // Extract the counter value
-        setCounterValue(value);
+    // Fetch data from the first source
+    fetch('https://www.free-counters.org/count/d1v9')
+      .then((response) => response.json())
+      .then((data) => {
+        // Handle data from the first source
+        setCounterData(data);
       })
-      .catch(error => {
-        console.error("Error fetching counter data:", error);
+      .catch((error) => {
+        console.error('Error fetching data:', error);
       });
+
+    // Fetch data from the second source
+    // You can use a similar approach for the second source
   }, []);
 
   return (
     <div>
-      <a href='https://www.free-counters.org/'>Counter</a>
-      <p>Counter Value: {counterValue !== null ? counterValue : "Loading counter data..."}</p>
+      {counterData ? (
+        <div>
+          <p>Counter Value: {counterData.count}</p>
+          {/* Render other data as needed */}
+        </div>
+      ) : (
+        <p>Loading data...</p>
+      )}
     </div>
   );
 }
