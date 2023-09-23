f878d38e27dc1766ba826ab808049d87068fc756
Franz Maneclang <104089786+NoobMaster-fsociety@users.noreply.github.com>
2023-09-23 21:36:56 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -16,26 +16,6 @@ function VisitCounter() {
   useEffect(() => {
     const storedVisits = Number(localStorage.getItem("visitCounter")) || 0;
     setVisits(storedVisits + 1);
-
-    // Create a script element for the first script
-    const script1 = document.createElement('script');
-    script1.type = 'text/javascript';
-    script1.src = 'https://www.freevisitorcounters.com/auth.php?id=70286397094907d2c90bc29f4cf13085b1f81001';
-    
-    // Create a script element for the second script
-    const script2 = document.createElement('script');
-    script2.type = 'text/javascript';
-    script2.src = 'https://www.freevisitorcounters.com/en/home/counter/1078731/t/0';
-
-    // Append the scripts to the document's head
-    document.head.appendChild(script1);
-    document.head.appendChild(script2);
-
-    // Clean up: remove the scripts when the component unmounts
-    return () => {
-      document.head.removeChild(script1);
-      document.head.removeChild(script2);
-    };
   }, []);
 
   // Saving in localStorage
@@ -43,5 +23,10 @@ function VisitCounter() {
     localStorage.setItem("visitCounter", visits);
   }, [visits]);
 
-  return <p>{visits}</p>;
+  return (
+    <div>
+      <a href="https://www.free-counters.org/">Counter</a>
+      <p>{visits}</p>
+    </div>
+  );
 }
